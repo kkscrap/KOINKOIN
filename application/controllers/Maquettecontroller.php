eec674d2e57113932ab17fbd361de6018200e071
kkscrap <delamorte.stephane@mail.com>
2017-02-03 21:54:00 +0100

    Ajout controlleur Maquette (pour les tests à venir)

---
+++ application/controllers/Maquettecontroller.php
@@ -0,0 +1,24 @@
+<?php
+
+class Maquettecontroller extends Zend_Controller_Action
+{
+	public function init()
+	{
+// 		throw new Exception("<h1>COQUILLE VIDE</h1>");
+	}
+	
+	public function indexAction()
+	{
+		// vue maquette/index.phtml
+	}
+	
+	public function testAction()
+	{
+		throw new Exception("<h1>COQUILLE VIDE</h1>");
+	}
+	
+	public function amauryAction()
+	{
+		// vue maquette/amaury.phtml
+	}	
+}

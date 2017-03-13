61b673f0e92dd23100a5645efca953651e1c0291
kkscrap <delamorte.stephane@mail.com>
2017-03-13 13:37:29 +0100

    Drop controleur

--- library/my_fonctions/my_fonctions_session.php
+++ library/my_fonctions/my_fonctions_session.php
@@ -1,25 +1,5 @@
 <?php
-
 function init_environnement_session()
 {
-// 	ini_set('display_errors','on');
-// 	error_reporting(E_ALL);
-	
-// 	$_SESSION['kk']['projet']['nom'] = 'koinkoin';
-// 	$_SESSION['kk']['debug']['verbose'] = 0;
-// 	$_SESSION['kk']['debug']['num'] = 0; // o = rien, ou choisir niveaux 1, 2 ou 3
-// 	$_SESSION['kk']['version']['nom_dossier'] = 'V';
-// 	$_SESSION['kk']['version']['num_a_utiliser'] = '0.0.01';
-// 	$_SESSION['kk']['version']['num_reel'] = '0.0.01';
-// 	$_SESSION['kk']['mes_fonctions']['prefixe_nom_fichier'] = 'MesFonctions';
-// 	$_SESSION['kk']['mes_fonctions']['nom_dossier'] = 'MesFonctions';
-// 	$_SESSION['kk']['multilangues']['actif'] = 0;
-// 	$_SESSION['kk']['multilangues']['langue_par_defaut'] = 'fr';
-// 	$_SESSION['kk']['multilangues']['langue_a_utiliser'] = 'espagnol';
-// 	$_SESSION['kk']['multilangues']['drapeau_fr'] = '../drapeau_fr_icone.gif';
-
-// 	$_SESSION['kk']['txts']['fr']['programme']['arret_du_programme']['version_specifiee']['invalide'] = 'ARRET DU PROGRAMME : la version specifiee de koinkoin n\'existe pas';
-// 	$_SESSION['kk']['txts']['espagnol']['programme']['arret_du_programme']['version_specifiee']['invalide'] = 'PROGRAMMA ES STOPA : el versionne de koinkoin no appariente de nostre';
 }
-
 ?>
